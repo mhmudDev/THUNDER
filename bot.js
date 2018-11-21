aed06b437c63f07a79970c363c8594e0fb45c092
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2018-11-21 21:04:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,41 @@
-const Discord = require('discord.js');
+onst Discord = require("discord.js");
 const client = new Discord.Client();
-const adminprefix = "*";
-const developers = ['488698374560677888'];
+var prefix = "*";
+var adminprefix = '*'
 
-console.log("Paaaaartly");
-
-client.on('ready', () => {
-    console.log(`Logged as ${client.user.tag}`)
-})
-
-
+const developers = ["488698374560677888","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'pl')) {
+  if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير البلاينق الى   ${argresult}**`)
+      message.channel.send(`**✅   ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
+  if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
+      message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
+  if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(`تم تغييرك حالتك بالتويتش الى   **${argresult}**`)
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith(adminprefix + 'sn')) {
+  if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم لـ ..**${argresult}** `)
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'sa')) {
+if (message.content.startsWith(adminprefix + 'setava')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`**جاري تغيير الأفتار... :** `);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
 
 
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
