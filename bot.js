ba2ea94367fbc4db26d412d5c2997a24dfba7439
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2018-11-22 12:38:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "*";
 var adminprefix = '*'
 
-const developers = ["488698374560677888","id"]
+const developers = ["488698374560677888"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
