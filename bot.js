bd7f7e3478872a6858558930b193c6179b2b8677
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-05-31 19:22:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,4 @@ client.on("guildMemberAdd", async member => {
 member.guild.channels.get('583623939503292426').send('**Welcome To Darkside. :sparkles:')
 });
 
-client.login('NTgzMDY3NjgyMTExMzU2OTM4.XO-kIA.WH95JROgPMiiAlkoFl0O5-zaEm0,NTgzNTkwNzk0NDAxOTM5NDY5.XO-lgw.P1Go1mUR9bGVHgu_LJeWJ3yWoIY');
+client.login('BOT_TOKEN');
