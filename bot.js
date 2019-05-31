44cf522388e93c9f386000f24657b8fb887f751a
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-05-31 19:15:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-    bot = new Discord.Client({sisableEveryone: true})
-    console.log("muuuuuuuuute,");
-bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("571169902975647804");
-if(!mohamed) return;
-if(mohamed) {
-setTimeout(() => mohamed.send(`Welcome To **Focus.**`), 4000)        
-}
+const prefix = '!';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+client.on("guildMemberAdd", async member => {
+member.guild.channels.get('583623939503292426').send('**Welcome To Darkside. :sparkles:')
+});
+
+client.login('NTgzMDY3NjgyMTExMzU2OTM4.XO-kIA.WH95JROgPMiiAlkoFl0O5-zaEm0,NTgzNTkwNzk0NDAxOTM5NDY5.XO-lgw.P1Go1mUR9bGVHgu_LJeWJ3yWoIY');
