f2d6068462fcba9fa99607898dc15b5c6e2348eb
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-05-31 22:43:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '!';
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("576374909496983582");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**Weclome To Darkside.** :sparkles:`), 4000)        
+}
 });
 
-client.on("guildMemberAdd", async member => {
-member.guild.channels.get('583623939503292426').send('**Welcome To Darkside. :sparkles:')
-});
-
-client.login('BOT_TOKEN');
+bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
